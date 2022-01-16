dc8aa42bbcbe7dc4cc52046bc2db6bdde7f9dc33
ArthurHtbk <arthur.heurtebise@gmail.com>
2022-01-16 11:11:42 +0100

    fix Project model

--- Models/Project.js
+++ Models/Project.js
@@ -3,16 +3,6 @@ const mongoose = require("mongoose");
 const Project = mongoose.model("Project", {
   image: { type: mongoose.Schema.Types.Mixed, default: {} },
   name: String,
-  description: {
-    english: String,
-    french: String,
-  },
-  tools: [String],
-  url: String,
-  repository: {
-    backend: String,
-    frontend: String,
-  },
 });
 
 module.exports = Project;
